d4347f43abfc3e857ae35db2ce8322e2b6f77e1e
Benjamín Montenegro <benjamin.montenegrom@gmail.com>
2023-11-23 22:58:36 -0300

    archivos de conf

---
+++ projects/collections/target/debug/deps/collections-091808ccec9e0f86.d
@@ -0,0 +1,5 @@
+C:\Repositorio\rust_book\projects\collections\target\debug\deps\libcollections-091808ccec9e0f86.rmeta: src\main.rs
+
+C:\Repositorio\rust_book\projects\collections\target\debug\deps\collections-091808ccec9e0f86.d: src\main.rs
+
+src\main.rs:

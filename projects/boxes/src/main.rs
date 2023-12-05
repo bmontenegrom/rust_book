50e28d17f95c7215c8f47ac254a538d80a037e3c
Benjamín Montenegro <benjamin.montenegrom@gmail.com>
2023-12-05 08:54:45 -0300

    par 497

--- projects/boxes/src/main.rs
+++ projects/boxes/src/main.rs
@@ -1,13 +1,57 @@
 use crate::List::{Cons, Nil};
-
+use std::ops::Deref;
 fn main() {
     let b = Box::new(5);
     println!("b = {b}");
 
     let _list = Cons(1, Box::new(Cons(2, Box::new(Cons(2, Box::new(Nil))))));
+
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let c = CustomSmartPointer{
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer{
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointers created!");
 }
 
 enum List {
     Cons(i32, Box<List>),
     Nil,
 }
+
+struct MyBox<T>(T);
+
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T>{
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+    
+}
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Droping CustomsmartPointer with data `{}`", self.data);
+    }
+    
+}
+
